2c9e1002c82569b2e027a01534d32ef3f2f90637
Addy Kim <soyaddyk@gmail.com>
2016-05-16 22:36:55 -0500

    Add subreddits

--- app/src/main/java/com/addyk/cat_viewer/Constants.java
+++ app/src/main/java/com/addyk/cat_viewer/Constants.java
@@ -4,8 +4,17 @@ public class Constants {
     public static final String MY_IMGUR_CLIENT_ID = "6566f5e4097334d";
     public static final String MY_IMGUR_CLIENT_SECRET = "a3fa24f690d31af352913a48a1660dc388da57cc";
     public static final String MY_IMGUR_REDIRECT_URL = "http://android";
-    public static final String IMGUR_CATS_URL = "https://imgur.com/r/blep/top.json";
-    public static final String IMGUR = "https://imgur.com/";
+    public static final String IMGUR = "https://imgur.com/r/";
+    public static final String TOP_JSON = "/top.json";
     public static final String ALBUM_NAME = "CatViewer";
     public static final int MAX_IMAGE_DIMEN = 4096;
+    public static final String[] IMGUR_CATS_URL = {
+            "blep",
+            "blop",
+            "Catpics",
+            "MEOW_IRL",
+            "TuckedInKitties",
+            "Catloaf"
+        };
+
 }
