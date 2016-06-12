ccdcaafc4ed390c70a10ffc989b52a78a6c2731c
Addy Kim <soyaddyk@gmail.com>
2016-06-11 17:38:43 -0700

    Add volley

--- app/src/main/java/com/addyk/cat_viewer/RequestImages.java
+++ app/src/main/java/com/addyk/cat_viewer/RequestImages.java
@@ -8,29 +8,29 @@ import android.graphics.Bitmap;
  */
 public class RequestImages {
     private static RequestImages mInstance;
-    private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
+//    private RequestQueue mRequestQueue;
+//    private ImageLoader mImageLoader;
     private static Context mCtx;
 
     private RequestImages(Context context) {
         mCtx = context;
-        mRequestQueue = getRequestQueue();
+//        mRequestQueue = getRequestQueue();
 
-        mImageLoader = new ImageLoader(mRequestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
+//        mImageLoader = new ImageLoader(mRequestQueue,
+//                new ImageLoader.ImageCache() {
+//                    private final LruCache<String, Bitmap>
+//                            cache = new LruCache<String, Bitmap>(20);
+//
+//                    @Override
+//                    public Bitmap getBitmap(String url) {
+//                        return cache.get(url);
+//                    }
+//
+//                    @Override
+//                    public void putBitmap(String url, Bitmap bitmap) {
+//                        cache.put(url, bitmap);
+//                    }
+//                });
     }
 
     public static synchronized RequestImages getInstance(Context context) {
@@ -40,21 +40,20 @@ public class RequestImages {
         return mInstance;
     }
 
-    public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-            // getApplicationContext() is key, it keeps you from leaking the
-            // Activity or BroadcastReceiver if someone passes one in.
-            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
-        }
-        return mRequestQueue;
-    }
-
-    public <T> void addToRequestQueue(Request<T> req) {
-        getRequestQueue().add(req);
-    }
-
-    public ImageLoader getImageLoader() {
-        return mImageLoader;
-    }
-}
+//    public RequestQueue getRequestQueue() {
+//        if (mRequestQueue == null) {
+//            // getApplicationContext() is key, it keeps you from leaking the
+//            // Activity or BroadcastReceiver if someone passes one in.
+//            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+//        }
+//        return mRequestQueue;
+//    }
+//
+//    public <T> void addToRequestQueue(Request<T> req) {
+//        getRequestQueue().add(req);
+//    }
+//
+//    public ImageLoader getImageLoader() {
+//        return mImageLoader;
+//    }
 }
